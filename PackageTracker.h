647b1480353f381aa7f1ec97ca42999302ccf8bb
Justin Knotzke <jknotzke@shampoo.ca>
2011-02-06 13:50:03 -0500

    Use a slightly safer max buffer size.

--- PackageTracker.h
+++ PackageTracker.h
@@ -8,7 +8,7 @@
 
 #define SCL_DELAY	1		//Delay to prevent reading the SCL sensor to fast
 #define	GPS_BUFFER_SIZE	80	//Size of buffer for GPS messages
-#define MAX_BUFFER_SIZE 800 
+#define MAX_BUFFER_SIZE 700 
 #define LOG_BUFFER_SIZE 1024
 #define OK	1
 #define ERROR	0
